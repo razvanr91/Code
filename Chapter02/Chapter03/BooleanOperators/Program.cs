5bad1398737a0fbd82e584d20589c3e80298e7d5
razvanr91 <razvanroman1991@gmail.com>
2022-01-12 13:25:47 +0000

    BooleanOperators

--- Chapter02/Chapter03/BooleanOperators/Program.cs
+++ Chapter02/Chapter03/BooleanOperators/Program.cs
@@ -4,14 +4,24 @@
 bool a = true;
 bool b = false;
 
-WriteLine($"AND  | a     | b    ");
-WriteLine($"a    | {a & a,-5} | {a & b,-5} ");
-WriteLine($"b    | {b & a,-5} | {b & b,-5}");
-WriteLine($"");
-WriteLine($"OR   | a     | b    ");
-WriteLine($"a    | {a | a,-5} | {a | b,-5}");
-WriteLine($"b    | {b | a,-5} | {b | b,-5}");
-WriteLine($"");
-WriteLine($"XOR  | a     | b    ");
-WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5}");
-WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5}");
+//WriteLine($"AND  | a     | b    ");
+//WriteLine($"a    | {a & a,-5} | {a & b,-5} ");
+//WriteLine($"b    | {b & a,-5} | {b & b,-5}");
+//WriteLine($"");
+//WriteLine($"OR   | a     | b    ");
+//WriteLine($"a    | {a | a,-5} | {a | b,-5}");
+//WriteLine($"b    | {b | a,-5} | {b | b,-5}");
+//WriteLine($"");
+//WriteLine($"XOR  | a     | b    ");
+//WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5}");
+//WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5}");
+
+
+static bool DoStuff()
+{
+    WriteLine("I am doing stuff.");
+    return true;
+}
+
+WriteLine($"a & DoStuff() = {a && DoStuff()}");
+WriteLine($"b & DoStuff() = {b && DoStuff()}");
