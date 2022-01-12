587da09200b252bbdd6ee351af3727b404f2e178
razvanr91 <razvanroman1991@gmail.com>
2022-01-12 13:18:30 +0000

    Boolean operators and operators

---
+++ Chapter02/Chapter03/BooleanOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+
+bool a = true;
+bool b = false;
+
+WriteLine($"AND  | a     | b    ");
+WriteLine($"a    | {a & a,-5} | {a & b,-5} ");
+WriteLine($"b    | {b & a,-5} | {b & b,-5}");
+WriteLine($"");
+WriteLine($"OR   | a     | b    ");
+WriteLine($"a    | {a | a,-5} | {a | b,-5}");
+WriteLine($"b    | {b | a,-5} | {b | b,-5}");
+WriteLine($"");
+WriteLine($"XOR  | a     | b    ");
+WriteLine($"a    | {a ^ a,-5} | {a ^ b,-5}");
+WriteLine($"b    | {b ^ a,-5} | {b ^ b,-5}");
---
+++ Chapter02/Chapter03/Operators/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+//int a = 3;
+//int b = a++;
+//WriteLine($"a is {a} and b is {b}");
+
+//int c = 3;
+//int d = ++c;
+//WriteLine($"c is {c} and d is {d}");
+
+int e = 11;
+int f = 3;
+//writeline($"e is {e}, f is {f}");
+//writeline($"e + f = {e + f}");
+//writeline($"e - f = {e - f}");
+//writeline($"e * f = {e * f}");
+//writeline($"e / f = {e / f}");
+//writeline($"e % f = {e % f}");
+
+double g = 11.0;
+//WriteLine($"g is {g:N1}, f is {f}");
+//WriteLine($"g /f = {g / f}");
+
