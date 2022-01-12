957ddb4172b909991ebcce2499bd325825428963
razvanr91 <razvanroman1991@gmail.com>
2022-01-12 13:42:05 +0000

    End

---
+++ Chapter02/Chapter03/BitwiseAndShiftOperatos/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+int a = 10;
+int b = 6;
+
+WriteLine($"a = {a}");
+WriteLine($"b = {b}");
+WriteLine($"a & b = {a & b}");
+WriteLine($"a | b = {a | b}");
+WriteLine($"a ^ b = {a ^ b}");
+
+WriteLine($"a << 3 = {a << 3}");
+WriteLine($"a * 8 = {a * 8}");
+WriteLine($"b >> 1 = {b >> 1}");
+
+WriteLine();
+WriteLine("Outputting integers as binary:");
+WriteLine($"a =      {ToBinaryString(a)}");
+WriteLine($"b =      {ToBinaryString(b)}");
+WriteLine($"a & b =  {ToBinaryString(a & b)}");
+WriteLine($"a | b =  {ToBinaryString(a | b)}");
+WriteLine($"a ^ b =  {ToBinaryString(a ^ b)}");
+
+static string ToBinaryString(int value)
+{
+    return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+}
