fab70aa130eb9992a448d4be88b771be7a03e2fa
razvanr91 <razvanroman1991@gmail.com>
2022-01-13 14:23:18 +0000

    Exercise 2 and 3

---
+++ Chapter02/Chapter03/Exercise03/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+for(int i = 1; i <= 100; i++)
+{
+    string result = i.ToString();
+
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        result = "FizzBuzz";
+    }
+    else if(i % 3 == 0)
+    {
+        result = "Fizz";
+    }
+    else if(i % 5 == 0)
+    {
+        result = "Buzz";
+    }
+
+    Console.Write($"{result} ");
+}
