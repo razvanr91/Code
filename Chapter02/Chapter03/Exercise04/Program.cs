de80172b031dfa3edd67dcdb93fc2c78105803d4
razvanr91 <razvanroman1991@gmail.com>
2022-01-13 14:33:12 +0000

    Exercise 4

---
+++ Chapter02/Chapter03/Exercise04/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Write("Enter a number between 0 and 255: ");
+string inputOne = ReadLine();
+WriteLine();
+
+Write("Enter another number between 0 and 255: ");
+string inputTwo = ReadLine();
+WriteLine();
+
+try
+{
+    int result = Convert.ToInt32(inputOne) / Convert.ToInt32(inputTwo);
+    WriteLine($"{inputOne} divided by {inputTwo} is {result}");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()}: {ex.Message}");
+}
