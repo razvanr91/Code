0f7956957d79cf2ccd29cf8f0422dcab969abb8f
razvanr91 <razvanroman1991@gmail.com>
2022-01-13 14:36:56 +0000

    Finish chapter 3

--- Chapter02/Chapter03/Exercise04/Program.cs
+++ Chapter02/Chapter03/Exercise04/Program.cs
@@ -1,19 +1,31 @@
 ﻿using static System.Console;
 
-Write("Enter a number between 0 and 255: ");
-string inputOne = ReadLine();
-WriteLine();
+//Write("Enter a number between 0 and 255: ");
+//string inputOne = ReadLine();
+//WriteLine();
 
-Write("Enter another number between 0 and 255: ");
-string inputTwo = ReadLine();
-WriteLine();
+//Write("Enter another number between 0 and 255: ");
+//string inputTwo = ReadLine();
+//WriteLine();
 
-try
-{
-    int result = Convert.ToInt32(inputOne) / Convert.ToInt32(inputTwo);
-    WriteLine($"{inputOne} divided by {inputTwo} is {result}");
-}
-catch (Exception ex)
-{
-    WriteLine($"{ex.GetType()}: {ex.Message}");
-}
+//try
+//{
+//    int result = Convert.ToInt32(inputOne) / Convert.ToInt32(inputTwo);
+//    WriteLine($"{inputOne} divided by {inputTwo} is {result}");
+//}
+//catch (Exception ex)
+//{
+//    WriteLine($"{ex.GetType()}: {ex.Message}");
+//}
+
+int x = 3;
+int y = 2 + ++x;
+Console.WriteLine($"{x} - {y}");
+
+x = 3 << 2;
+y = 10 >> 1;
+Console.WriteLine($"{x} - {y}");
+
+x = 10 & 8;
+y = 10 | 7;
+Console.WriteLine($"{x} - {y}");
