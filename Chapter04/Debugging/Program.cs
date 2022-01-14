26fa10aa41b7c99e47eec08a3382e1a1184485b1
razvanr91 <razvanroman1991@gmail.com>
2022-01-14 14:34:26 +0000

    Debugging

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+static double Add(double a, double b)
+{
+    return a + b; // deliberate bug
+}
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a,b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press enter to end the app.");
+ReadLine();
