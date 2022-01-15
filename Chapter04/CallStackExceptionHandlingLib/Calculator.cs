21fd96f2fabcca1c6f145c9a6831dca8bf0a875e
razvanr91 <razvanroman1991@gmail.com>
2022-01-15 23:34:57 +0000

    CallStackExceptionHandlingLib

---
+++ Chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Packt
+{
+    public class Calculator
+    {
+        public static void Gamma()
+        {
+            WriteLine("In Gamma");
+            Delta();
+        }
+
+        private static void Delta()
+        {
+            WriteLine("In Delta");
+            File.OpenText("bag file path");
+        }
+    }
+}
