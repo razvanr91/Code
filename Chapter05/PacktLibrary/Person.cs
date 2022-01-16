a52fcae8442afd9db5656ecd6e380c9e806faca0
razvanr91 <razvanroman1991@gmail.com>
2022-01-16 17:27:06 +0000

    Chapter 5

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -1,8 +1,20 @@
-﻿using static System.Console;
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace Packt.Shared
 {
     public class Person : object
     {
+        public string Name;
+
+        public DateTime DateOfBIrth;
+
+
+        public List<Person> Children = new List<Person>();
+
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+
+        public WondersOfTheAncientWorld BucketList;
     }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,6 +1,40 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 using Packt.Shared;
 
 Person bob = new();
 
-WriteLine(bob.ToString());
+bob.Name = "Bob Smith";
+bob.DateOfBIrth = new DateTime(1965, 12, 22);
+
+//WriteLine($"{bob.Name} was born on {bob.DateOfBIrth:dddd, d MMMM yyyy}");
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    DateOfBIrth = new(1998, 3, 7)
+};
+
+//WriteLine($"{alice.Name} was born on {alice.DateOfBIrth:dd MM yy}.");
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+
+WriteLine($"{bob.Name}`s favorite wonder is {bob.FavoriteAncientWonder}. Its integer is {(int)bob.FavoriteAncientWonder}.");
+
+bob.BucketList = WondersOfTheAncientWorld.HangingGardensOfBabylon | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+
+WriteLine($"{bob.Name}`s bucket list is {bob.BucketList}.");
+
+bob.Children.Add(new Person { Name = "Alfred" });
+bob.Children.Add(new() { Name = "Zoe" });
+
+WriteLine($"{bob.Name} has {bob.Children.Count} children:");
+for(int i = 0; i < bob.Children.Count; i++)
+{
+    WriteLine($"    {bob.Children[i].Name}");
+}
+
+foreach(Person child in bob.Children)
+{
+    WriteLine(child.Name);
+}
