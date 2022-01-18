c8e704a875b5e0b1dd422917178be5607f35fb4d
razvanr91 <razvanroman1991@gmail.com>
2022-01-18 01:19:29 +0000

    Pattern matching enhancements in C# 9 and above

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -153,9 +153,17 @@ foreach(object passenger in passengers)
 {
     decimal flightCost = passenger switch
     {
-        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
-        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
-        FirstClassPassenger _                         => 2000M,
+        // C# 8 syntax
+        //FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        //FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        //FirstClassPassenger _                         => 2000M,
+        //C# 9 syntax
+        FirstClassPassenger p => p.AirMiles switch 
+        {
+            > 35000 => 1500M,
+            > 15000 => 1750M,
+            _       => 2000M
+        },
         BusinessClassPassenger _                      => 1000M,
         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
         CoachClassPassenger _                         => 650M,
