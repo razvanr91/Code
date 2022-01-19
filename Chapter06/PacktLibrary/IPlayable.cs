eca989af597d71b2d30ef6e6e47b23fd1394c2c5
razvanr91 <razvanroman1991@gmail.com>
2022-01-19 02:11:53 +0000

    Define interfaces with default implementations

---
+++ Chapter06/PacktLibrary/IPlayable.cs
@@ -0,0 +1,13 @@
+﻿namespace Packt.Shared;
+
+public interface IPlayable
+{
+    void Play();
+
+    void Pause();
+
+    void Stop()
+    {
+        System.Console.WriteLine("Default implementation of Stop.");
+    }
+}
