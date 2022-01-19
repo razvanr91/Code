7518f937c5cad60b8d652a167c47e978f6223819
razvanr91 <razvanroman1991@gmail.com>
2022-01-19 20:56:03 +0000

    Extension method to reuse finctionality

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -140,3 +140,14 @@ catch (PersonException ex)
 {
     WriteLine(ex.Message);
 }
+
+string email1 = "pamela@test.com";
+string email2 = "ian&test.com";
+
+// Before static extension of method
+//WriteLine($"{email1} is a valid e-mail address: {StringExtensions.IsValidEmail(email1)}");
+//WriteLine($"{email2} is a valid e-mail address: {StringExtensions.IsValidEmail(email2)}");
+
+// After static extension of method
+WriteLine($"{email1} is a valid e-mail address: {email1.IsValidEmail()}");
+WriteLine($"{email2} is a valid e-mail address: {email2.IsValidEmail()}");
