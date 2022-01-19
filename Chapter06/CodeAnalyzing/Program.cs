7b4f90ba0618fd605afac32c1e429347f2ba4cff
razvanr91 <razvanroman1991@gmail.com>
2022-01-19 21:24:11 +0000

    End of chapter 6

--- Chapter06/CodeAnalyzing/Program.cs
+++ Chapter06/CodeAnalyzing/Program.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Packt">
+// Copyright (c) Packt. All rights reserved.
+// </copyright>
 
 namespace CodeAnalyzing
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    using System;
+    /// <summary>
+    /// The main class for this console app.
+    /// </summary> 
+
+    public class Program
+    { 
+        /// <summary>
+        /// The main entry point for this console app.
+        /// </summary>
+        /// <param name="args">A string array of arguments passed to the console app.</param>
+        
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
